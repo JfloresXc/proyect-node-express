1bcfebceb5bd87f5203e74967430adc72366c020
JfloresXc <72885001+JfloresXc@users.noreply.github.com>
2020-11-22 16:14:24 -0500

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -9,8 +9,4 @@ router.get('/contact', (req, res) => {
     res.render('contact.html',{'title':'Contact'})
 })
 
-router.get('/prueba', (req, res) => {
-    res.render('prueba.html')
-})
-
 module.exports = router
